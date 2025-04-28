72bbb5800cc62c5ee01c71347aaec9c0e9f56cfd
jowjow22 <jonathan224santos@gmail.com>
2025-04-28 15:28:11 -0300

    feat: create base service with mock

---
+++ lib/services/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/user_model.dart';
+
+const mockUserData = '''
+{
+  "id": "1",
+  "name": "John Doe",
+  "email": "jhon@gmail.com",
+  "password": "password123"
+}
+''';
+
+class ApiService {
+  final _baseUrl = 'example';
+
+  Future<User> getUserData(String userId) async {
+    return User.fromJson(jsonDecode(mockUserData));
+  }
+}
